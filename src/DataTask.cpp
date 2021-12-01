483b6b7c47f7c1afb368df06cc63d36cb669a3f9
Cole Stanton <chstanton99@gmail.com>
2021-11-30 18:36:25 -0800

    Started Data task .cpp and .h

---
+++ src/DataTask.cpp
@@ -0,0 +1,8 @@
+/** @file DataTask.cpp
+ *    
+ * 
+ *  @author Cole Stanton
+ *  @date   2021-Nov-30 Original file
+ */
+
+#include "DataTask.h"
---
+++ src/DataTask.h
@@ -0,0 +1,8 @@
+/** @file DataTask.h
+ *    
+ * 
+ *  @author Cole Stanton
+ *  @date   2021-Nov-30 Original file
+ */
+
+#include <Arduino.h>
