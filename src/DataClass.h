19541d5330dd82a3b633b7fe25b7182b8bd7a036
Cole Stanton <chstanton99@gmail.com>
2021-12-01 17:58:22 -0800

    Added Data class and edited data task

---
+++ src/DataClass.h
@@ -0,0 +1,17 @@
+/** @file DataTask.cpp
+ *    
+ * 
+ *  @author Cole Stanton
+ *  @date   2021-Nov-30 Original file
+ */
+
+#include <vector>
+
+class DataClass
+{
+    protected:
+    public:
+    void Input_Data(char);
+    void Output_Data(void);
+
+};
--- src/DataTask.cpp
+++ src/DataTask.cpp
@@ -11,8 +11,7 @@
     #include <STM32FreeRTOS.h>
 #endif
 #include "DataTask.h"
-#include "square_data.csv"
-#include "fstream.h"
+#include <fstream>
 
 /** @brief   
  *  @details 
@@ -28,46 +27,8 @@ void task_show_data (void* p_params)
         // In state 0 we're waiting for signal from control task to pull data 
         if (state == 0)
         {
-            if (pull_data.get ())
-            {
-
-                fstream input;
-                input.open("square_data.csv", ios::in);
-
-                // Read the Data from the file
-                // as String Vector
-                vector<string> row;
-                string line, word, temp;
-            
-                while (input >> temp) 
-                {
-            
-                    row.clear();
-            
-                    // read an entire row and
-                    // store it in a string variable 'line'
-                    getline(input, line);
-            
-                    // used for breaking words
-                    stringstream s(line);
-            
-                    // read every column data of a row and
-                    // store it in a string variable, 'word'
-                    while (getline(s, word, ', ')) {
-            
-                        // add all the column data
-                        // of a row to a vector
-                        row.push_back(word);
-                    }
-
-
-                while()
-                pull_data.put (false);      // So we don't record forever
-                counter = 0;
-                state = 1;
-            }
+            state = 1;
         }
-
         // In state 1 data is being recorded until the queue is empty
         else if (state == 1)
         {
@@ -75,8 +36,9 @@ void task_show_data (void* p_params)
             data_queue.put (analogRead (DATA_ACQ_PIN));
 
             // Check if we've taken the required number of data points yet
-            if (++counter > POINTS_PER_SET)
+            if (encoder_queue.get())
             {
+                
                 state = 0;
             }
         }
