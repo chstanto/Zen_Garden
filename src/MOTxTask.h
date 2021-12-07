74223b7ff9e882c6d73a548e0b9b3dd45b1f2f28
Cole Stanton <chstanton99@gmail.com>
2021-12-06 16:44:01 -0800

    Revert "documentation"
    This reverts commit 83556983c5219fd4265e4f548400706d838b57f0.

--- src/MOTxTask.h
+++
@@ -1,7 +0,0 @@
-/** @file MOTxTask.h
- *  This file contains a task for controlling the motor in the x direction
- *  @author  Aaron Tran
- *  @date    2021-Nov-30 Original file
- */
-
-void task_MOTx(void* p_params);
--- src/MOTyTask.h
+++
@@ -1,7 +0,0 @@
-/** @file MOTyTask.h
- *  This file contains a task for controlling the motor in the y direction
- *  @author  Aaron Tran
- *  @date    2021-Nov-30 Original file
- */
-
-void task_MOTy(void* p_params);
