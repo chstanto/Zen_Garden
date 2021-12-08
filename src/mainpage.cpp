0b645e25bb97b93d723c1bb06dd84812e8b1faf7
malkstik <52295451+malkstik@users.noreply.github.com>
2021-12-07 18:45:59 -0800

    documentation update

---
+++ src/mainpage.cpp
@@ -0,0 +1,11 @@
+/*! \mainpage Mechatronic Zen Garden Documentation
+ *
+ * \section intro_sec Introduction
+ *
+ * This project was completed for the ME507: Mechanical Control Systems Design course at California Polytechnic State University, San Luis Obispo by Aaron Tran and Cole Stanton.
+ * All code is written in C++ and designed to run on the STM32 NUCLEO-L476RG. \n
+ * \n
+ * For more information, refer to the following links \n
+ *     - GitHub:    https://github.com/chstanto/Zen_Garden
+ *     - Report:    https://cpslo-my.sharepoint.com/:b:/g/personal/atran189_calpoly_edu/ET6GacYjFDJDtd2uNoskVV0BfOKDRYbpZIQnxC-zn9sTSg?e=dDn6c3
+ */
